fe8162c1eadf454be8cd3a77065804522bba5f1b
Temidayo Gabriel <dayotoru@gmail.com>
2025-11-13 20:11:05 -0500

    implement tanstack query for both client and server components

---
+++ app/tanstack-provider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { QueryClientProvider } from "@tanstack/react-query";
+import { type ReactNode } from "react";
+import { getQueryClient } from "./get-query-client";
+
+export default function TanstackProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const queryClient = getQueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
